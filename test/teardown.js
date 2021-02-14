4b7fefe6cc69deb39d55b042fbecc8a9979c482a
Matteo Collina <hello@matteocollina.com>
2021-02-14 02:34:07 +0100

    teardown

--- test/teardown.js
+++ test/teardown.js
@@ -28,3 +28,22 @@ tap.test('multiple teardown', {timeout: 1000}, function (tt) {
         t.end();
     });
 });
+
+tap.test('teardown with promise', {timeout: 1000}, function (tt) {
+    tt.plan(2);
+    tape('dummy test', function (t) {
+        var resolved = false;
+        t.teardown(function () {
+            tt.pass('tape teardown');
+            var p = Promise.resolve();
+            p.then(function () {
+                resolved = true;
+            });
+            return p;
+        });
+        t.on('end', function () {
+            tt.is(resolved, true);
+        });
+        t.end();
+    });
+});
